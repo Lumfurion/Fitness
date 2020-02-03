af29d0c5d83d7d14149b115c124156d91f839738
Vladimir <esel10@ukr.net>
2020-02-03 17:33:23 +0200

    Улучшен интерфейс окна регистрации.

--- Fitness.Wpf/Enter to account/Registration.xaml.cs
+++ Fitness.Wpf/Enter to account/Registration.xaml.cs
@@ -1,17 +1,9 @@
 ﻿using Fitness.BusinessLogic.Controller;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
+
 
 namespace Fitness.Wpf
 {
@@ -19,12 +11,21 @@ namespace Fitness.Wpf
     public partial class Registration : Window
     {
         UserController user { get; set; }
+        string Gender { get; set; }
         MainWindow signIn { get; set; }
         public Registration()
         {
             InitializeComponent();
             signIn = new MainWindow();
-            reg.Closed += Reg_Closed;    
+            reg.Closed += Reg_Closed;
+
+            rbМan.Checked += RadioButton_Checked;
+            rbWoman.Checked += RadioButton_Checked;
+        }
+        private void RadioButton_Checked(object sender, RoutedEventArgs e)
+        {
+            RadioButton pressed = (RadioButton)sender;
+            Gender = pressed.Content.ToString();
         }
 
         private void Reg_Closed(object sender, EventArgs e)
@@ -32,9 +33,19 @@ namespace Fitness.Wpf
             signIn.Show();
         }
 
-        private void TbSave_Click(object sender, RoutedEventArgs e)
+        private void btnCloseOnClick(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void titleBar_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            DragMove();
+        }
+
+        private void tbSaveOnClick(object sender, RoutedEventArgs e)
         {   //Проверка.
-           
+
             user = new UserController(tbUsername.Text);
             if (!user.isNewUser)
             {
@@ -42,18 +53,20 @@ namespace Fitness.Wpf
             }
             else if (user.isNewUser == true)
             {
-                var password = tbPassword.Password;
-                var gender = tbGender.Text;
-                var birtdayDate = Convert.ToDateTime(tbbirthdayDate.Text);
+                var password = tbPassword.Text;
+                var gender = Gender;
+                var date = tbdateday.Text + "." + tbdatemonth.Text + "." + tbdateyear.Text;
+                var birtdayDate = Convert.ToDateTime(date);
                 var weight = Convert.ToInt32(tbweight.Text);
                 var height = Convert.ToInt32(tbheight.Text);
-                user.SetNewUserData(gender,password,birtdayDate,weight,height);
+                user.SetNewUserData(gender, password, birtdayDate, weight, height);
 
                 signIn.Show();
                 Close();
             }
 
-
         }
+
+       
     }
 }
